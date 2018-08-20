fb5abc5a0c837c23d32e7c2379057efeac25ff70
YunJiHun <yjhzzing4699@naver.com>
2018-08-20 23:06:29 +0900

    countdowntestllll

---
+++ js/vendor/countdown.js
@@ -0,0 +1,35 @@
+CountDownTimer('13/10/2018', 'countdown'); // 2017년 1월 1일까지
+CountDownTimer('13/10/2018 11:00 AM', 'newcountdown'); // 2018년 1월 1일까지, 시간을 표시하려면 01:00 AM과 같은 형식을 사용합니다.
+function CountDownTimer(dt, id)
+{
+var end = new Date(dt);
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+var now = new Date();
+var distance = end - now;
+if (distance < 0) {
+
+clearInterval(timer);
+document.getElementById(id).innerHTML = 'EXPIRED!';
+
+return;
+}
+var days = Math.floor(distance / _day);
+var hours = Math.floor((distance % _day) / _hour);
+var minutes = Math.floor((distance % _hour) / _minute);
+var seconds = Math.floor((distance % _minute) / _second);
+
+document.getElementById(id).innerHTML = days + '일 ';
+document.getElementById(id).innerHTML += hours + '시간 ';
+document.getElementById(id).innerHTML += minutes + '분 ';
+document.getElementById(id).innerHTML += seconds + '초';
+}
+
+timer = setInterval(showRemaining, 1000);
+}
